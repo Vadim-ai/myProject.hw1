9918a5b35694dc8d6aee7fd1fde4957297eeef97
SkyPro <vadim.phobia@gmail.com>
2022-04-15 23:26:36 +0300

    Сделанное задание от Вадима

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("вроде все правильно");//очень странное ДЗ
+        System.out.println("вроде все правильно");//очень странное ДЗ..
     }
 }
